b0ccf01963efe962257b6c9e62cbe1f301b31fbb
dennissch <dpcmlake01@gmail.com>
2023-02-01 21:06:41 -0800

    Arm Telemtry quick fix

--- src/main/java/frc/robot/arm/ArmTelemetry.java
+++ src/main/java/frc/robot/arm/ArmTelemetry.java
@@ -12,9 +12,9 @@ public class ArmTelemetry {
 
     public ArmTelemetry() {
         tab = Shuffleboard.getTab("Arm");
-        tab.addString("Lower Limit Switch",()-> Robot.arm.lowerLimitSwitchStatus()).withSize(3, 2).withPosition(0, 0);
-        tab.addString("Upper Limit Switch",()-> Robot.arm.UpperLimitSwitchStatus()).withSize(3, 2).withPosition(0, 4);
-        tab.addDouble("Encoder Cnt", ()->Robot.arm.getEncoder())                   .withSize(3, 3).withPosition(0, 6);
-        WidgetsAndLayouts.TalonFXLayout("Motor", tab, Robot.arm.mArmMotor)         .withSize(3, 5).withPosition(6, 0);
+        tab.addString("Lower Limit Switch",()-> Robot.arm.lowerLimitSwitchStatus()).withPosition(0, 0).withSize(3, 2);
+        tab.addString("Upper Limit Switch",()-> Robot.arm.UpperLimitSwitchStatus()).withPosition(0, 4).withSize(3, 2);
+        tab.addDouble("Encoder Cnt", ()->Robot.arm.getEncoder())                   .withPosition(0, 6).withSize(3, 2);
+        WidgetsAndLayouts.TalonFXLayout("Motor", tab, Robot.arm.mArmMotor)         .withPosition(5, 0).withSize(6, 6);
     }
 }
