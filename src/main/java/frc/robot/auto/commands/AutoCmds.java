b64f60bcdbc463c5d59c2deefd4eddf6541b0b59
dennissch <dpcmlake01@gmail.com>
2023-02-19 18:11:27 -0800

    addional cmd update

--- src/main/java/frc/robot/auto/commands/AutoCmds.java
+++ src/main/java/frc/robot/auto/commands/AutoCmds.java
@@ -28,14 +28,14 @@ public class AutoCmds {
             // 4. Turn on arm brake mode
             ArmCmds.SetArmBrakeCmd(),
             // 5. raise arm to target pos
-            ArmCmds.ArmSetMMangle(Auto.armPosition).withTimeout(2),
+            ArmCmds.ArmSetMMangleCmd(Auto.armPosition).withTimeout(2),
             // 6. elev lower to final target pos
             ElevatorCmds.setMMPosition(Auto.elevEndPos).withTimeout(2),
             // 7. eject
             IntakeCmds.IntakeEjectCmd(),
             // 8. Intake Arm to get ready for following path
             ElevatorCmds.setMMPosition(AutoConfig.kElevTop).withTimeout(2),
-            ArmCmds.ArmSetMMangle(ArmConfig.ArmAngleStorePos),
+            ArmCmds.ArmSetMMangleCmd(ArmConfig.ArmAngleStorePos),
             ElevatorCmds.ElevGoToBottomCmd().withTimeout(2)
         );
     }
