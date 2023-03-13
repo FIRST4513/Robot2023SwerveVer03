3d7964136acee939ae093742448d1608b73438cf
Dyllan Ruggles <dyllruggs@gmail.com>
2023-03-13 16:49:37 -0700

    Added LedSubsytem method setLEDs

--- src/main/java/frc/robot/leds/LedConfig.java
+++ src/main/java/frc/robot/leds/LedConfig.java
@@ -1,6 +1,9 @@
 package frc.robot.leds;
 
 public class LedConfig {
+    public int ledPort = 9;
+    public int ledAmount = 60;
+
     public LedConfig() {
     }
 }
--- src/main/java/frc/robot/leds/LedSubSys.java
+++ src/main/java/frc/robot/leds/LedSubSys.java
@@ -1,15 +1,21 @@
 package frc.robot.leds;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LedSubSys extends SubsystemBase {
-    public static LedConfig config;
-    public static String intakeBrakeStatus = "";
+    public LedConfig config;
     
     // Devices
+    public PWMVictorSPX lightStrip;
 
     // Constructor
     public LedSubSys() { 
-    } 
+        config = new LedConfig();
+        lightStrip = new PWMVictorSPX(5);
+    }
 
+    public void setLEDs(double value) {
+        lightStrip.set(value);
+    }
 }
